e5f29573ce1e0378eac2ee1e50279cf2ea4afb98
Michal Suszczewicz <m.suszczewicz@intive.com>
2021-02-17 17:34:31 +0100

    merge reports branch into report html branch

--- parser.py
+++ parser.py
@@ -216,12 +216,6 @@ def validate_metrics(report_file_name):
 
     import webbrowser
     new = 2  # open in a new tab, if possible
-
-    # open a public URL, in this case, the webbrowser docs
-    # url = "http://docs.python.org/library/webbrowser.html"
-    # webbrowser.open(url,new=new)
-
-    # open an HTML file on my own (Windows) computer
     url = "file://" + os.getcwd() + "/results.html"
     webbrowser.open(url, new=new)
     
